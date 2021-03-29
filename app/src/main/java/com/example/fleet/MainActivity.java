7f5091bbbdc76af26b33285a3759b1a6fc26fdce
Samuel Tocci <samuel.tocci@gmail.com>
2021-03-29 03:02:55 +0200

    Facebook login werkt, profile pic en naam wordt getoond, vriendenlijst wordt ook gefetched

--- app/src/main/java/com/example/fleet/MainActivity.java
+++ app/src/main/java/com/example/fleet/MainActivity.java
@@ -2,13 +2,107 @@ package com.example.fleet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+        textView = findViewById(R.id.tv_name);
+        imageView = findViewById(R.id.iv_profilepic);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setPermissions(Arrays.asList("user_friends"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("demo ", "login successful");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("demo ", "login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("demo ", "login error");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d("demo", object.toString());
+                try {
+                    String name = object.getString("name");
+                    String id = object.getString("id");
+                    textView.setText(name);
+                    Picasso.get().load("https://graph.facebook.com/"+ id + "/picture?type=large")
+                            .into(imageView);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "name, id, first_name, last_name, friends");
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+    }
+
+    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null){
+                LoginManager.getInstance().logOut();
+                textView.setText("");
+                imageView.setImageResource(0);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 }
--- app/src/main/java/com/example/fleet/User.java
+++ app/src/main/java/com/example/fleet/User.java
@@ -1,8 +1,12 @@
 package com.example.fleet;
 
+import android.media.Image;
+
 public class User {
     private String first_name;
     private String last_name;
+    private String nick_name;
+    private Image pfPic;
 
     public User(String first_name, String last_name) {
         this.first_name = first_name; // naam uiteindelijk uit Fb API halen

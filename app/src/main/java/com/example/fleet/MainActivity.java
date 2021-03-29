8f4960297bc0e318363f89deb9f48f8a3a3543d1
Samuel Tocci <samuel.tocci@gmail.com>
2021-03-29 22:14:16 +0200

    color scheme en button toegevoegd

--- app/src/main/java/com/example/fleet/MainActivity.java
+++ app/src/main/java/com/example/fleet/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -18,6 +20,8 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -31,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private ImageView iv_profilePic;
     private TextView tv_name;
+
+    // Use the FloatingActionButton for all the add person
+    // and add alarm
+    FloatingActionButton mAddAlarmFab, mAddPersonFab;
+
+    // Use the ExtendedFloatingActionButton to handle the
+    // parent FAB
+    ExtendedFloatingActionButton mAddFab;
+
+    // These TextViews are taken to make visible and
+    // invisible along with FABs except parent FAB's action
+    // name
+    TextView addAlarmActionText, addPersonActionText;
+
+    // to check whether sub FABs are visible or not
+    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +80,107 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("demo ", "login error");
             }
         });
+
+        // Register all the FABs with their appropriate IDs
+        // This FAB button is the Parent
+        mAddFab = findViewById(R.id.add_fab);
+        // FAB button
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        mAddPersonFab = findViewById(R.id.add_person_fab);
+
+        // Also register the action name text, of all the
+        // FABs. except parent FAB action name text
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        addPersonActionText = findViewById(R.id.add_person_action_text);
+
+        // Now set all the FABs and all the action name
+        // texts as GONE
+        mAddAlarmFab.setVisibility(View.GONE);
+        mAddPersonFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        addPersonActionText.setVisibility(View.GONE);
+
+        // make the boolean variable as false, as all the
+        // action name texts and all the sub FABs are
+        // invisible
+        isAllFabsVisible = false;
+
+        // Set the Extended floating action button to
+        // shrinked state initially
+        mAddFab.shrink();
+
+        // We will make all the FABs and action name texts
+        // visible only when Parent FAB button is clicked So
+        // we have to handle the Parent FAB button first, by
+        // using setOnClickListener you can see below
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+
+                            // when isAllFabsVisible becomes
+                            // true make all the action name
+                            // texts and FABs VISIBLE.
+                            mAddAlarmFab.show();
+                            mAddPersonFab.show();
+                            addAlarmActionText.setVisibility(View.VISIBLE);
+                            addPersonActionText.setVisibility(View.VISIBLE);
+
+                            // Now extend the parent FAB, as
+                            // user clicks on the shrinked
+                            // parent FAB
+                            mAddFab.extend();
+
+                            // make the boolean variable true as
+                            // we have set the sub FABs
+                            // visibility to GONE
+                            isAllFabsVisible = true;
+                        } else {
+
+                            // when isAllFabsVisible becomes
+                            // true make all the action name
+                            // texts and FABs GONE.
+                            mAddAlarmFab.hide();
+                            mAddPersonFab.hide();
+                            addAlarmActionText.setVisibility(View.GONE);
+                            addPersonActionText.setVisibility(View.GONE);
+
+                            // Set the FAB to shrink after user
+                            // closes all the sub FABs
+                            mAddFab.shrink();
+
+                            // make the boolean variable false
+                            // as we have set the sub FABs
+                            // visibility to GONE
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+        // below is the sample action to handle add person
+        // FAB. Here it shows simple Toast msg. The Toast
+        // will be shown only when they are visible and only
+        // when user clicks on them
+        mAddPersonFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Person Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // below is the sample action to handle add alarm
+        // FAB. Here it shows simple Toast msg The Toast
+        // will be shown only when they are visible and only
+        // when user clicks on them
+        mAddAlarmFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override

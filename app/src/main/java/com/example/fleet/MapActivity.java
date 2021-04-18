eec9e53b54323837d51141b051de2eaf1cc377a6
Samuel Tocci <samuel.tocci@gmail.com>
2021-04-18 02:06:40 +0200

    overgang tussen MapActivity en GroupActivity gemaakt

--- app/src/main/java/com/example/fleet/MapActivity.java
+++ app/src/main/java/com/example/fleet/MapActivity.java
@@ -1,18 +1,30 @@
 package com.example.fleet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
-
+    private ImageView groups_button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_view);
 
+        groups_button = findViewById(R.id.groups);
+        groups_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapActivity.this, GroupActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, R.anim.slide_out_right);
+            }
+        });
     }
 
 

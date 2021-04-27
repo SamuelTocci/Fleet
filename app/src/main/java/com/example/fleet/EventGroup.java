39c24e0ccfed250d77d73e68e60310c0b9607f51
Samuel Tocci <samuel.tocci@gmail.com>
2021-04-27 13:33:28 +0200

    parcelable abstract Event classe gemaakt en 2 subclasses

---
+++ app/src/main/java/com/example/fleet/EventGroup.java
@@ -0,0 +1,8 @@
+package com.example.fleet;
+
+public class EventGroup extends Event {
+
+    public EventGroup(String id, String group_name, String description) {
+        super(id, group_name, description);
+    }
+}
---
+++ app/src/main/java/com/example/fleet/EventTemp.java
@@ -0,0 +1,9 @@
+package com.example.fleet;
+
+public class EventTemp extends Event {
+
+    public EventTemp(String id, String group_name, String description) {
+        super(id, group_name, description);
+    }
+}
+
